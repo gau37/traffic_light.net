747ce20246ff64a518edc39d7798557809a39b96
gau37 <139674193+gau37@users.noreply.github.com>
2023-07-21 19:10:26 +0900

    Update style.js

--- docs/src/style.js
+++ docs/src/style.js
@@ -7,7 +7,7 @@ const article = document.querySelector('article');
 
 main.insertAdjacentHTML(
   'beforebegin', 
-  '<header><h1 class="image_title"><a href="https://gau37.github.io/traffic_light.net/"><img src="https://gau37.github.io/traffic_light.net/pictures/icon.png" alt="traffic_light_" /></a></h1><nav class="pc_nav"><ul class="menu"><li><a href="#" class="mein">概要</a><ul class="menu_2"><li><a href="https://gau37.github.io/traffic_light.net/%E6%A6%82%E8%A6%81/%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB">プロフィール</a></li><li><a href="#">ここについて</a></li></ul></li><li><a href="#" class="mein">記録</a><ul class="menu_2"><li><a href="#">独り言</a></li><li><a href="#">制作物</a></li><li><a href="#">更新履歴</a></li></ul></li></ul></nav></header>');
+  '<header><h1 class="image_title"><a href="https://gau37.github.io/traffic_light.net/"><img src="https://gau37.github.io/traffic_light.net/pictures/icon.png" alt="traffic_light_" /></a></h1><nav class="pc_nav"><ul class="menu"><li><a href="#" class="mein">概要</a><ul class="menu_2"><li><a href="https://gau37.github.io/traffic_light.net/%E6%A6%82%E8%A6%81/%E3%83%97%E3%83%AD%E3%83%95%E3%82%A3%E3%83%BC%E3%83%AB">プロフィール</a></li><li><a href="https://gau37.github.io/traffic_light.net/%E6%A6%82%E8%A6%81/%E3%81%93%E3%81%93%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6">ここについて</a></li></ul></li><li><a href="#" class="mein">記録</a><ul class="menu_2"><li><a href="#">独り言</a></li><li><a href="#">制作物</a></li><li><a href="#">更新履歴</a></li></ul></li></ul></nav></header>');
 
 
 article.insertAdjacentHTML(
